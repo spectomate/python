f032d78a191d219afe39a95758a4c870be9f428b
Tom Softreck <info@softreck.dev>
2025-05-05 16:18:04 +0200

    Release version 0.1.61
    ### Added
    - Changes in .github/workflows/python-package.yml
    - Changes in pyproject.toml
    - Changes in pyproject.toml.bak
    - Changes in spectomate/__init__.py
    - Changes in spectomate/__init__.py.bak

--- spectomate/__init__.py
+++ spectomate/__init__.py
@@ -4,7 +4,7 @@ Spectomate - modularny konwerter pakietów Python
 Narzędzie do konwersji między różnymi formatami zarządzania pakietami w Pythonie.
 """
 
-__version__ = "0.1.24"
+__version__ = "0.1.25"
 __author__ = "Tom Sapletta"
 
 from spectomate.core.base_converter import BaseConverter
